da8df4058d910b097502a661117cf82e90143c32
Emmanuel Imolorhe <imolorhe@gmail.com>
2024-08-29 22:20:55 +0100

    persist state to local storage

--- src/components/kanbanBoard/contexts.tsx
+++ src/components/kanbanBoard/contexts.tsx
@@ -1,57 +1,65 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { UniqueIdentifier } from "@dnd-kit/core";
 import { CardType } from "@/components/kanbanBoard/types";
 
-const initialCards: CardType[] = [
+const defaultCards: CardType[] = [
   {
     id: "card1",
     listId: "todo",
-    title: "First",
+    title: "Prepare Project Proposal",
     priority: "LOW",
-    dueDate: new Date().toString(),
-    description: "Task 1",
-    imageSrc: "https://picsum.photos/seed/picsum/200/300",
+    dueDate: new Date("2024-09-10T10:00:00Z").toISOString(),
+    description: "Draft the initial proposal for the upcoming project meeting.",
+    imageSrc: "https://picsum.photos/seed/project1/200/300",
   },
   {
     id: "card2",
     listId: "inprogress",
-    title: "Firstond",
+    title: "Design User Interface",
     priority: "MEDIUM",
-    dueDate: new Date(1600000000000).toString(),
-    description: "Task 2",
-    imageSrc: "https://picsum.photos/seed/picsum/200/300",
+    dueDate: new Date("2024-09-15T14:00:00Z").toISOString(),
+    description:
+      "Create wireframes and mockups for the new application interface.",
+    imageSrc: "https://picsum.photos/seed/design/200/300",
   },
   {
     id: "card3",
     listId: "done",
-    title: "Ford",
+    title: "Fix Bug in Authentication",
     priority: "HIGH",
-    dueDate: new Date(1800000000000).toString(),
-    description: "Task 3",
+    dueDate: new Date("2024-08-25T09:00:00Z").toISOString(),
+    description: "Resolve the issue with user login that prevents access.",
+    imageSrc: "https://picsum.photos/seed/bugfix/200/300",
   },
   {
     id: "card4",
     listId: "done",
-    title: "Feet",
+    title: "Update Documentation",
     priority: "LOW",
-    dueDate: new Date().toString(),
-    description: "Task 4",
+    dueDate: new Date("2024-08-20T11:00:00Z").toISOString(),
+    description:
+      "Revise the project's documentation to reflect recent changes.",
+    imageSrc: "https://picsum.photos/seed/documentation/200/300",
   },
   {
     id: "card5",
     listId: "done",
-    title: "Seat",
+    title: "Conduct User Testing",
     priority: "MEDIUM",
-    dueDate: new Date(1850000000000).toString(),
-    description: "Task 5",
+    dueDate: new Date("2024-08-30T13:00:00Z").toISOString(),
+    description:
+      "Organize and execute user testing sessions to gather feedback.",
+    imageSrc: "https://picsum.photos/seed/testing/200/300",
   },
   {
     id: "card6",
     listId: "done",
-    title: "Se'un",
+    title: "Deploy Application",
     priority: "HIGH",
-    dueDate: new Date(1720000000000).toString(),
-    description: "Task 6",
+    dueDate: new Date("2024-09-01T16:00:00Z").toISOString(),
+    description:
+      "Deploy the final version of the application to the production environment.",
+    imageSrc: "https://picsum.photos/seed/deploy/200/300",
   },
 ];
 
@@ -77,6 +85,14 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
   const [activeCardId, setActiveCardId] = useState<UniqueIdentifier | null>(
     null
   );
+
+  const localStorageCards: CardType[] = JSON.parse(
+    localStorage.getItem("cards") || "[]"
+  );
+  const initialCards = localStorageCards?.length
+    ? localStorageCards
+    : defaultCards;
+
   const [cards, setCards] = useState<CardType[]>(initialCards);
   const [currentListId, setCurrentListId] = useState("");
   const [currentCard, setCurrentCard] = useState<CardType | null>(null);
@@ -84,6 +100,12 @@ export const AppProvider = ({ children }: { children: React.ReactNode }) => {
 
   const handleOpenModal = () => setIsModalOpen(true);
   const handleCloseModal = () => setIsModalOpen(false);
+
+  useEffect(() => {
+    const cardString = JSON.stringify(cards || []);
+
+    localStorage.setItem("cards", cardString);
+  }, [cards]);
 
   return (
     <AppContext.Provider
